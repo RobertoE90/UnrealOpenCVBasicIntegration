f6e399ce9938ef98403b85af302ad0b2c25edf5c
Roberto Estrada <robertoestradaleyva@gmail.com>
2020-09-27 09:53:09 -0400

    intial commit

---
+++ Source/UnrealOpenCvTest/UnrealOpenCvTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealOpenCvTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALOPENCVTEST_API AUnrealOpenCvTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
